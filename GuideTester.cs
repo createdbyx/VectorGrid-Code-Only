80c6597fbaed46b511ff8c93c762302bddcfd9f6
Dean Lunz <deanlunz@createdbyx.com>
2019-02-19 16:02:16 -0800

    minor changes

--- GuideTester.cs
+++ GuideTester.cs
@@ -11,11 +11,11 @@
         public void OnGUI()
         {
             var guide = this.GetComponent<Guides>();
-            var text = guide.HighlightPosition.ToString();          
+            var text = guide.HighlightPosition.ToString();
             var rect = new Rect(100, 100, 100, 100);
             var mousePosition = Input.mousePosition;
             rect.x = mousePosition.x - 50;
-            rect.y = -mousePosition.y - 25;
+            rect.y = Screen.height - mousePosition.y - 25;
             GUI.Label(rect, text);
         }
     }
